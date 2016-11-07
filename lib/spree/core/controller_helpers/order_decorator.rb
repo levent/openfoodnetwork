8137d9f3de0656ad8000acf0fac6f85ac1ba34f4
Levent Ali <lebreeze@gmail.com>
2016-11-07 18:02:12 +0000

    Stop overriding set_current_order to retrieve last incomplete order

--- lib/spree/core/controller_helpers/order_decorator.rb
+++ lib/spree/core/controller_helpers/order_decorator.rb
@@ -12,20 +12,4 @@ Spree::Core::ControllerHelpers::Order.class_eval do
     order
   end
   alias_method_chain :current_order, :scoped_variants
-
-
-  # Override definition in spree/auth/app/controllers/spree/base_controller_decorator.rb
-  # Do not attempt to merge incomplete and current orders. Instead, destroy the incomplete orders.
-  def set_current_order
-    if user = try_spree_current_user
-      last_incomplete_order = user.last_incomplete_spree_order
-
-      if session[:order_id].nil? && last_incomplete_order
-        session[:order_id] = last_incomplete_order.id
-
-      elsif current_order && last_incomplete_order && current_order != last_incomplete_order
-        last_incomplete_order.destroy
-      end
-    end
-  end
 end
