503cc961d28161855ad7e4a0f8f7dca22d3ead8c
Levent Ali <lebreeze@gmail.com>
2016-11-07 16:41:41 +0000

    Introduce failing spec on last_incomplete_spree_order

--- config/initializers/user_class_extensions.rb
+++ config/initializers/user_class_extensions.rb
@@ -3,9 +3,10 @@ Spree::Core::Engine.config.to_prepare do
     Spree.user_class.class_eval do
 
       # Override of spree method to ignore orders associated with account_invoices
-      def last_incomplete_spree_order
+      def last_incomplete_spree_order_with_ignoring_account_invoices
         spree_orders.incomplete.where("id NOT IN (?)", account_invoices.map(&:order_id)).order('created_at DESC').first
       end
+      alias_method_chain :last_incomplete_spree_order, :ignoring_account_invoices
     end
   end
 end
